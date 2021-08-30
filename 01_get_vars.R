abb5c0b71cb7ae7511243a57736113912a9db0ef
Simon Brewer <brewersi@gmail.com>
2021-08-30 13:11:47 -0600

    Updated paths and general clean up

--- 01_get_vars.R
+++ 01_get_vars.R
@@ -1,13 +1,13 @@
 library(sf)
 library(raster)
 
-glac_sf <- st_read("../data/glacier.shp")
+glac_sf <- st_read("./data/glacier.shp")
 
-t2m <- stack('../era5/t2m_2001_2020.nc', varname = 't2m')
+t2m <- stack('~/Dropbox/Data/summer/era5/t2m_2001_2020.nc', varname = 't2m')
 glac_t2m <- extract(t2m, glac_sf)
 glac_t2m <- glac_t2m - 273.15
 
-tp <- stack('../era5/download.nc', varname = 'tp')
+tp <- stack('~/Dropbox/Data/summer/era5/download.nc', varname = 'tp')
 glac_tp <- extract(tp, glac_sf)
 glac_tp <- glac_tp * 24*60*60
 
@@ -23,26 +23,3 @@ glac_sf$area_km2 <- glac_sf$area_m2 / 1e6
 
 st_write(glac_sf, "../data/glacier_clim.shp", update = TRUE)
 
-## Anomaly check
-t2m_ann <- stackApply(t2m, rep(c(1,2), each = 12), mean)
-
-t2m_anm <- raster(t2m_ann, 2) - raster(t2m_ann, 1)
-plot(t2m_anm)
-
-
-stop()
-x <- stack("../era5/tmp.nc")
-
-y1 <- c(t2m_ann[700, 1])
-y2 <- c(x[700, 1])
-
-plot(y1, y2)
-
-yrs <- seq(1, 20)
-summary(lm(y1 ~ yrs))
-summary(lm(y2 ~ yrs))
-
-bb <- raster("../era5/t2m_b.nc")
-
-bb[700, 1]
-t2m_ann <- stackApply(t2m, rep(seq(1,20), each = 12), mean)
