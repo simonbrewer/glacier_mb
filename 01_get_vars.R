dd3f6ea83762594d40f9e4eabb415496c4008d03
Simon Brewer <brewersi@gmail.com>
2021-09-03 19:32:59 -0600

    Added delta on precipitation seasonality

--- 01_get_vars.R
+++ 01_get_vars.R
@@ -30,6 +30,7 @@ glac_sf$tpjja_18 <- apply(glac_tpjja[, 16:20], 1, mean)
 ## Seasonality
 glac_sf$tpseas_08 <- glac_sf$tpjja_08 / glac_sf$tp_08
 glac_sf$tpseas_18 <- glac_sf$tpjja_18 / glac_sf$tp_18
+glac_sf$tpseas_d <- glac_sf$tpseas_18 - glac_sf$tpseas_08
 
 seas_df <- data.frame(tp_seas = c(glac_sf$tpseas_08, glac_sf$tpseas_18),
                       time = rep(c("2008", "2018"), each = nrow(glac_sf)))
