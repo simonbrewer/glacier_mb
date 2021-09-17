3dea236b8b49a0e84b8088018697b28b7cceee28
simonbrewer <brewersi@gmail.com>
2021-09-17 16:51:26 -0600

    Attempted to make graph on CHPC

--- 00_make_graph.R
+++ 00_make_graph.R
@@ -20,11 +20,11 @@ dat <- dat_sf %>%
 coords <- cbind(dat$east / 1000,
              dat$north / 1000)
 
-# system.time(nb_gabriel <- gabrielneigh(coords))
-# nb <- graph2nb(nb_gabriel, sym = TRUE)
+system.time(nb_gabriel <- gabrielneigh(coords))
+nb <- graph2nb(nb_gabriel, sym = TRUE)
 
-system.time(nb_knn <- col.knn <- knearneigh(coords, k=4))
-nb <- knn2nb(col.knn, sym = TRUE)
+# system.time(nb_knn <- col.knn <- knearneigh(coords, k=4))
+# nb <- knn2nb(col.knn, sym = TRUE)
 
 nb2INLA("map.adj", nb)
 
